71e0a94139cce305e810afbbfc6b16a2bc24bf4a
mihajlotrajkovic <mihajlotrajkovic@gmail.com>
2026-06-10 16:06:12 +0200

    done

--- server1.py
+++ server1.py
@@ -1,32 +1,31 @@
 # Server2 šalje Serveru1 vreme poslednje izmene svog data.txt. Server1 proverava da li je njegov fajl noviji.
 # Ako jeste, ispisuje poruku: „Server1 ima noviju verziju“. Ako nije, ispisuje „Server2 ima noviju verziju“ ili „Fajlovi su sinhronizovani“ ako je vreme isto.
 
-import hashlib
+#prvo se pokrece server2 pa server1
+
+import os
 import socket
 
-def hashiranje(putanja):
-    sha256 = hashlib.sha256()
-    with open(putanja, "rb") as f:
-        for blok in iter(lambda: f.read(4096), b""):
-            sha256.update(blok)
-    return sha256.hexdigest()
+def vreme_izmene(putanja):
+    return os.path.getmtime(putanja)
 
 def main():
-    moj_hash = hashiranje("data1.txt")
+    moje_vreme = vreme_izmene("data1.txt")
+    # moje_vreme = vreme_izmene("data2.txt") # ovde ce biti sinhronizovani
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("127.0.0.1", 5000))
 
-    s.sendall(moj_hash.encode())
+    s.sendall(str(moje_vreme).encode())
 
-    hash_server2 = s.recv(1024).decode()
+    vreme_server2 = float(s.recv(1024).decode())
 
-    if moj_hash >= hash_server2:
+    if moje_vreme > vreme_server2:
         print("Server1 ima noviju verziju")
-    elif moj_hash <= hash_server2:
+    elif moje_vreme < vreme_server2:
         print("Server2 ima noviju verziju")
     else:
-        print("Fajlovi nisu identični")
+        print("Fajlovi su sinhronizovani")
 
     s.close()
 
--- server2.py
+++ server2.py
@@ -1,18 +1,14 @@
 # Server2 šalje Serveru1 vreme poslednje izmene svog data.txt. Server1 proverava da li je njegov fajl noviji.
 # Ako jeste, ispisuje poruku: „Server1 ima noviju verziju“. Ako nije, ispisuje „Server2 ima noviju verziju“ ili „Fajlovi su sinhronizovani“ ako je vreme isto.
 
-import hashlib
+import os
 import socket
 
-def hashiranje(putanja):
-    sha256 = hashlib.sha256()
-    with open(putanja, "rb") as f:
-        for blok in iter(lambda: f.read(4096), b""):
-            sha256.update(blok)
-    return sha256.hexdigest()
+def vreme_izmene(putanja):
+    return os.path.getmtime(putanja)
 
 def main():
-    moj_hash = hashiranje("data2.txt")
+    moje_vreme = vreme_izmene("data2.txt")
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("0.0.0.0", 5000))
@@ -23,9 +19,9 @@ def main():
     conn, addr = s.accept()
     print(f"Povezan sa {addr}")
 
-    hash_server1 = conn.recv(1024).decode()
+    vreme_server1 = float(conn.recv(1024).decode())
 
-    conn.sendall(moj_hash.encode())
+    conn.sendall(str(moje_vreme).encode())
 
     conn.close()
     s.close()
